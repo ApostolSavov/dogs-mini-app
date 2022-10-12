c71eaab8d3067ca918d655cb0f9a3738d1da183a
Apostol Savov <apostol.savov.97@gmail.com>
2022-10-12 21:54:34 +0300

    Remove unused component and import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useEffect, useState } from 'react';
 import { getMaxRandomImages } from './services/images';
 
